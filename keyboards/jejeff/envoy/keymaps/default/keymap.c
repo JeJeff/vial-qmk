bd333b7b4b165f806ae9c8d48cf5af2797b68c55
JeJeff <35032770+JeJeff@users.noreply.github.com>
2021-10-31 14:21:01 +0000

    first vial version

---
+++ keyboards/jejeff/envoy/keymaps/default/keymap.c
@@ -0,0 +1,134 @@
+/* Copyright 2021 JeJeff
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+// Defines names for use in layer keycodes and the keymap
+enum layer_names {
+    _BASE
+    /*,
+    _FN*/
+};
+
+
+// XXXXXXX is No Op
+// _______ is transparaent
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* Base */
+    [_BASE] = LAYOUT(
+        KC_BSPACE,      KC_KP_SLASH,    KC_KP_ASTERISK, KC_KP_MINUS,    KC_MUTE,
+        KC_KP_7,        KC_KP_8,        KC_KP_9,        KC_KP_PLUS,     KC_MUTE,
+        KC_KP_4,        KC_KP_5,        KC_KP_6,        KC_KP_PLUS,     KC_MUTE,
+        KC_KP_1,        KC_KP_2,        KC_KP_3,        KC_KP_ENTER,    KC_MUTE,
+        KC_KP_0,        KC_KP_0,        KC_KP_DOT,      KC_KP_ENTER
+    )
+    /*,
+    [_FN] = LAYOUT(
+        TG(_FN),        KC_KP_SLASH,    KC_KP_ASTERISK, KC_KP_MINUS,    XXXXXXX,
+        KC_KP_7,        KC_KP_8,        KC_KP_9,        KC_KP_PLUS,     XXXXXXX,
+        KC_KP_4,        KC_KP_5,        KC_KP_6,        KC_KP_PLUS,     XXXXXXX,
+        KC_KP_1,        KC_KP_2,        KC_KP_3,        KC_KP_ENTER,    XXXXXXX,
+        KC_KP_0,        KC_KP_0,        KC_KP_DOT,      KC_KP_ENTER
+    )*/
+};
+
+
+
+#ifdef ENCODER_ENABLE
+// Useful examples of using encoders can be found at 
+// https://docs.splitkb.com/hc/en-us/articles/360010513760-How-can-I-use-a-rotary-encoder-
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) {
+        if (clockwise) {
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
+        }
+    } else if (index == 1) {
+        if (clockwise) {
+            tap_code(KC_PGDN);
+        } else {
+            tap_code(KC_PGUP);
+        }
+    } else if (index == 2) {
+        if (clockwise) {
+            tap_code(KC_MNXT);
+        } else {
+            tap_code(KC_MPRV);
+        }
+    } else if (index == 3) {
+        if (clockwise) {
+            tap_code(KC_MFFD);
+        } else {
+            tap_code(KC_MRWD);
+        }
+    }
+    return true;
+}
+#endif
+
+
+/*
+// Defines the keycodes used by our macros in process_record_user
+enum custom_keycodes {
+    QMKBEST = SAFE_RANGE,
+    QMKURL
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case QMKBEST:
+            if (record->event.pressed) {
+                // when keycode QMKBEST is pressed
+                SEND_STRING("QMK is the best thing ever!");
+            } else {
+                // when keycode QMKBEST is released
+            }
+            break;
+        case QMKURL:
+            if (record->event.pressed) {
+                // when keycode QMKURL is pressed
+                SEND_STRING("https://qmk.fm/\n");
+            } else {
+                // when keycode QMKURL is released
+            }
+            break;
+    }
+    return true;
+}
+*/
+
+
+// I hate the red LEDs showing on the pro micro, do you?
+void matrix_init_kb(void) {
+
+    // // green led on
+    // DDRD |= (1<<5);
+    // PORTD &= ~(1<<5);
+
+    // // orange led on
+    // DDRB |= (1<<0);
+    // PORTB &= ~(1<<0);
+	
+	//turn off LEDs on ProMicro
+   DDRD &= ~(1<<5);
+   PORTD &= ~(1<<5);
+
+   DDRB &= ~(1<<0);
+   PORTB &= ~(1<<0);
+
+	matrix_init_user();
+};
