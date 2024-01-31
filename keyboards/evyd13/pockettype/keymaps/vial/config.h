a49743e4e202d49ff320f8a12bcd720d99745eb1
JeJeff <35032770+JeJeff@users.noreply.github.com>
2024-01-31 18:23:57 +0000

    add vial firmware for pockettype

---
+++ keyboards/evyd13/pockettype/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xB2, 0x05, 0x89, 0xF2, 0x20, 0xCC, 0xE9, 0x4C}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 2 }
